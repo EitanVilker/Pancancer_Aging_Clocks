50575e617f63c16a8464ef0963e1891d4a6783cf
Eitan Vilker <evilker@bu.edu>
2025-02-03 13:57:40 -0500

    Added imputation fix back in, reformatted survival analysis, improved covariate handling, made cross-validation round two actually good

--- scripts/run_analysis_pipeline.R
+++ scripts/run_analysis_pipeline.R
@@ -1,58 +1,60 @@
-run_analysis_pipeline <- function(fullExperiment, prediction_df, prediction_object=NULL) {
-  library(dplyr)
-  library(survival)
-  library(SummarizedExperiment)
-  library(ggplot2)
-  library(reactable)
-  library(caret)
-  library(glmnet)
-  library(dplyr)
-  library(impute)
-  library(survival)
-  library(survminer)
-  library(vip)
-  library(tidyverse)
-  
-  # Step 1: Create Metadata Table
-  make_meta_df <- function(fullExperiment) {
-    metadata_table <- data.frame(submitter_id = rownames(fullExperiment@colData))
-    for (name in names(fullExperiment@colData@listData)) {
-      metadata_table[[name]] <- fullExperiment@colData@listData[[name]]
-    }
-    metadata_table[is.na(metadata_table)] <- 0
-    return(metadata_table)
+# library(dplyr)
+# library(survival)
+# library(SummarizedExperiment)
+# library(ggplot2)
+# library(reactable)
+# library(caret)
+# library(glmnet)
+# library(dplyr)
+# library(impute)
+# library(survival)
+# library(survminer)
+# library(vip)
+# library(tidyverse)
+
+# Step 1: Create Metadata Table
+make_meta_df <- function(fullExperiment) {
+  metadata_table <- data.frame(submitter_id = rownames(fullExperiment@colData))
+  for (name in names(fullExperiment@colData@listData)) {
+    metadata_table[[name]] <- fullExperiment@colData@listData[[name]]
   }
-  
-  # Step 2: Prepare Survival Data
-  create_DFsurv <- function(predicted_ages, metadata_table, delta_age_thresh = 0) {
-    #if (!identical(rownames(predicted_ages), rownames(metadata_table))) {
-    #  stop("Row names of predicted_ages and metadata_table must match.")
-    #}
-    metadata_table$predicted_age <- predicted_ages$predicted_age
-    metadata_table$delta_age <- metadata_table$predicted_age - metadata_table$Age
-    DFsurv <- metadata_table |>
-      dplyr::select(submitter_id, Age, predicted_age, vital_status, delta_age, Survival_Time, gender, HPV.status) |>
-      dplyr::rename(
-        chronological = Age,
-        predicted = predicted_age,
-        vitals = vital_status,
-        fu = Survival_Time
-      ) |>
-      dplyr::mutate(meta_age = factor(
-        dplyr::case_when(
-          delta_age > delta_age_thresh ~ "older",
-          delta_age < delta_age_thresh ~ "younger",
-          TRUE ~ "same"
-        ), levels = c("younger", "older", "same")
-      )) |>
-      dplyr::mutate(age_strata = cut(
-        chronological,
-        breaks = c(0, 50, 70, 90, +Inf),
-        labels = c("0-50", "50-70", "70-90", "90+")
-      ))
-    DFsurv$surv <- survival::Surv(time = DFsurv$fu, event = as.numeric(factor(DFsurv$vitals)) - 1)
-    return(DFsurv)
-  }
+  metadata_table[is.na(metadata_table)] <- 0
+  return(metadata_table)
+}
+
+# Step 2: Prepare Survival Data
+create_DFsurv <- function(predicted_ages, metadata_table, delta_age_thresh = 0) {
+  #if (!identical(rownames(predicted_ages), rownames(metadata_table))) {
+  #  stop("Row names of predicted_ages and metadata_table must match.")
+  #}
+  metadata_table$predicted_age <- predicted_ages$predicted_age
+  metadata_table$delta_age <- metadata_table$predicted_age - metadata_table$Age
+  DFsurv <- metadata_table |>
+    dplyr::select(submitter_id, Age, predicted_age, vital_status, delta_age, Survival_Time, gender, HPV.status) |>
+    dplyr::rename(
+      chronological = Age,
+      predicted = predicted_age,
+      vitals = vital_status,
+      fu = Survival_Time
+    ) |>
+    dplyr::mutate(meta_age = factor(
+      dplyr::case_when(
+        delta_age > delta_age_thresh ~ "older",
+        delta_age < delta_age_thresh ~ "younger",
+        TRUE ~ "same"
+      ), levels = c("younger", "older", "same")
+    )) |>
+    dplyr::mutate(age_strata = cut(
+      chronological,
+      breaks = c(0, 50, 70, 90, +Inf),
+      labels = c("0-50", "50-70", "70-90", "90+")
+    ))
+  DFsurv$surv <- survival::Surv(time = DFsurv$fu, event = as.numeric(factor(DFsurv$vitals)) - 1)
+  return(DFsurv)
+}
+
+# Step 3: Run the CoxPH analysis
+run_analysis_pipeline <- function(fullExperiment, prediction_df, prediction_object=NULL, covariates_to_include=c("gender", "race")) {
   
   # Prepare Metadata Table
   metadata_table <- make_meta_df(fullExperiment)
@@ -70,23 +72,29 @@ run_analysis_pipeline <- function(fullExperiment, prediction_df, prediction_obje
   
   # Create Survival Dataset
   experiment_prediction_object <- create_DFsurv(predicted_ages, filtered_metadata)
-  
+
   experiment_prediction_object_meta <- experiment_prediction_object %>%
     inner_join(filtered_metadata, by = "submitter_id")
   
   # Set up formulas, accounting for covariates appropriate to the cancer
-  formula_vector_non_interaction <- c("chronological + delta_age", "race")
-  formula_vector_interaction <- c("chronological * delta_age", "race")
-  formula_vector_baseline <- c("chronological", "race")
-  if (is.null(prediction_object) || "gendermale" %in% colnames(prediction_object)){
+  formula_vector_non_interaction <- c("chronological + delta_age")
+  formula_vector_interaction <- c("chronological * delta_age")
+  formula_vector_baseline <- c("chronological")
+  
+  if (nlevels(as.factor(experiment_prediction_object_meta$gender.y)) > 1){
     formula_vector_non_interaction <- c(formula_vector_non_interaction, "gender.y")
     formula_vector_interaction <- c(formula_vector_interaction, "gender.y")
     formula_vector_baseline <- c(formula_vector_baseline, "gender.y")
   }
+  if (nlevels(as.factor(experiment_prediction_object_meta$race)) > 1){
+    formula_vector_non_interaction <- c(formula_vector_non_interaction, "race")
+    formula_vector_interaction <- c(formula_vector_interaction, "race")
+    formula_vector_baseline <- c(formula_vector_baseline, "race")
+  }
   covariates_formula_non_interaction <- as.formula(paste("surv ~ ", paste(formula_vector_non_interaction, collapse= "+")))
   covariates_formula_interaction <- as.formula(paste("surv ~ ", paste(formula_vector_interaction, collapse= "+")))
   covariates_formula_baseline <- as.formula(paste("surv ~ ", paste(formula_vector_baseline, collapse= "+")))
-  
+
   # Run Non-Interaction CoxPH Model
   test1_non_interaction <- coxph(
     covariates_formula_non_interaction,
